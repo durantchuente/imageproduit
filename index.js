26771401dfac5d4ad3379f7f493449f2e9006dff
duran tchuente <duran.tchuente@gmail.com>
2020-02-25 22:32:06 +0100

    push reussi

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.post('/chatfuel', function (req, res) {
 
 function display_products(id_produit_index,produits) {
     
-    kota++;
+    kota=0;
     var id_produit = produits[id_produit_index].id;
     // a=id_produit;
     GetJson('http://localhost/prestashop/api/products/'+id_produit+'?ws_key='+tokenprestashop+'&output_format=JSON',function (err,produit) {
